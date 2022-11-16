641a41e3a2b21042ad1a444fdb8c6a696c53c881
Ivan Comi <ivan04comi@gmail.com>
2022-11-16 20:08:35 +0100

    Version 3.0: Stable
    Stable server that handles html, css, js and image file format.

--- WebServer/WebServer/Form1.Designer.cs
+++ WebServer/WebServer/Form1.Designer.cs
@@ -28,15 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnStart = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nClients = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nClients)).BeginInit();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(377, 200);
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(202, 362);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(82, 43);
+            this.btnStart.Size = new System.Drawing.Size(168, 76);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "START";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -44,25 +59,168 @@
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(250, 200);
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(12, 362);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(82, 43);
+            this.btnStop.Size = new System.Drawing.Size(171, 76);
             this.btnStop.TabIndex = 1;
             this.btnStop.Text = "STOP";
             this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click_1);
             // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(99, 232);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(48, 22);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "80";
+            this.txtPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 234);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(114, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "WebNett";
+            // 
+            // nClients
+            // 
+            this.nClients.Location = new System.Drawing.Point(99, 273);
+            this.nClients.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nClients.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nClients.Name = "nClients";
+            this.nClients.Size = new System.Drawing.Size(48, 20);
+            this.nClients.TabIndex = 5;
+            this.nClients.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Max Clients";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBrowse.BackgroundImage")));
+            this.btnBrowse.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnBrowse.Location = new System.Drawing.Point(122, 71);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(31, 32);
+            this.btnBrowse.TabIndex = 7;
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            this.btnBrowse.MouseEnter += new System.EventHandler(this.btnBrowse_MouseEnter);
+            this.btnBrowse.MouseHover += new System.EventHandler(this.btnBrowse_MouseHover);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "HTML Data Path";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Current Path:";
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblPath.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPath.Location = new System.Drawing.Point(12, 137);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(0, 18);
+            this.lblPath.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(198, 233);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 19);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Status:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(254, 233);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 19);
+            this.lblStatus.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(382, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nClients);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "WebNett Server";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nClients)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +228,18 @@
 
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nClients;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- WebServer/WebServer/Form1.cs
+++ WebServer/WebServer/Form1.cs
@@ -11,6 +11,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
 
 namespace WebServer
 {
@@ -21,21 +23,24 @@ namespace WebServer
             InitializeComponent();
         }
 
-        public static int clients = 10;
         public static List<Thread> threads = new List<Thread>();
+        public static int maxClients=-1;
         public static bool started = false;
-        //public static Dictionary<string, string> login = new Dictionary<string, string>();
-        //public static Dictionary<string, int> cookies = new Dictionary<string, int>();
+        public static int port=-1;
 
-        //public static string accessList = @"access.csv";
         public static string directory = "config";
         public static string log = @"config/server.log";
-        public static string wwwData = "www";
-        //public static string cookie = @"config/cookie.conf";
+        public static string www=null;
 
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            btnStop.Enabled = false;
+            lblPath.Text = "Non Selezionato";
+
+            lblStatus.Text = "Inactive";
+            lblStatus.ForeColor = Color.Orange;
+
             try
             {
                 if (!Directory.Exists(directory))
@@ -45,32 +50,27 @@ namespace WebServer
             }
             catch (Exception a)
             {
-                MessageBox.Show("Errore nella creazione della prima configurazione! \n" + a.ToString(), "!ERRORE!");
+                MessageBox.Show("Errore nella creazione della prima configurazione!\n\n" + a.ToString(), "!ERRORE!");
             }
         }
-
 
         class Listener
         {
             public void startListening()
             {
-                IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
-                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 80);
+                IPAddress ipAddress = IPAddress.Parse("10.0.0.98");
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
 
                 Socket listener = new Socket(ipAddress.AddressFamily,
                     SocketType.Stream, ProtocolType.Tcp);
 
-                Console.WriteLine("Timeout : {0}", listener.ReceiveTimeout);
-
                 try
                 {
                     listener.Bind(localEndPoint);
-                    listener.Listen(clients);
+                    listener.Listen(maxClients);
 
                     while (true)
                     {
-                        Console.WriteLine("Waiting for a connection...");
-
                         Socket handler = listener.Accept();
 
                         Manager manager = new Manager(handler);
@@ -81,7 +81,6 @@ namespace WebServer
                 }
                 catch (Exception a)
                 {
-                    MessageBox.Show(a.ToString(), "!ERRORE!");
                     File.AppendAllText(log, DateTime.Now.ToString() + "\tError\t" + a.ToString() + "\n");
                 }
             }
@@ -107,11 +106,9 @@ namespace WebServer
         class Manager
         {
             Socket client;
-            byte[] bytes = new byte[1024];
+            byte[] bytes = new byte[4096];
             string dataReceive = "";
             string dataSend = "";
-            //string http = @"http.txt";
-
 
             public Manager(Socket client)
             {
@@ -122,82 +119,165 @@ namespace WebServer
             {
                 byte[] msg;
                 int bytesRec;
-                string root = "/index.html";
+                string root = "index.html";
+                string[] lines = null;
+                string[] requested = null;
+                string document = null;
+                int length = -1;
 
                 bytesRec = client.Receive(bytes);
                 dataReceive = Encoding.ASCII.GetString(bytes, 0, bytesRec);
 
-                string[] lines = dataReceive.Split('\n');   //Divido tutto il contenuto della richiesta, utilizzando '\n'
-                string[] requested = lines[0].Split((char)32);   //Divido il contenuto della prima riga, utilizzando ' '
-
-                //Console.WriteLine(lines[0]);
-                //foreach (var i in requested)
-                //Console.WriteLine(i);
-
-                string line=null;
-
-
-                if (requested[1]=="/")
+                lines = dataReceive.Split('\n');
+                if (lines[0].StartsWith("GET /"))
                 {
-                    if (File.Exists(wwwData + root))
+                    lock (lines)
                     {
-                        line = File.ReadAllText(wwwData + root);
-                        int lenght = line.Length;
-                        dataSend = "HTTP/1.1 200 OK\r\nDate: "+DateTime.Now.ToString()+"\r\nServer: WebNett\r\nLast-Modified: "+File.GetLastWriteTime(wwwData + root).ToString()+"\r\nContent-Lenght: " + lenght + "\r\nKeep-Alive: timeout=10, max=100\r\nConnection: Keep-Alive\r\nContent-Type: text/html\r\n" + line;
-                    }
-                    else
-                    {
-                        dataSend = "HTTP/1.1 404 Not found\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nKeep-Alive: timeout=10, max=100\r\nConnection: Keep-Alive\r\nContent-Type: text/html\r\n";
+                        requested = lines[0].Split(' ');
+                        if (requested[1] == "/")
+                        {
+                            if (File.Exists(www + root))
+                            {
+                                document = File.ReadAllText(www + root);
+                                length = document.Length;
+                                dataSend = "HTTP/1.1 200 OK\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nLast-Modified: " + File.GetLastWriteTime(www + root).ToString() + "\r\nContent-Lenght: " + length + "\r\nContent-Type: text/html\r\n\r\n" + document + "\n\n";
+                            }
+                            else
+                                dataSend = "HTTP/1.1 404 Not found\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nContent-Type: text/html\r\n\r\n";
+                        }
+                        if (requested[1].Contains(".html"))
+                        {
+                            if (File.Exists(www + requested[1]))
+                            {
+                                document = File.ReadAllText(www + requested[1]);
+                                length = document.Length;
+                                dataSend = "HTTP/1.1 200 OK\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nLast-Modified: " + File.GetLastWriteTime(www + requested[1]).ToString() + "\r\nContent-length: " + length + "\r\nContent-Type: text/html\r\n\r\n" + document + "\n\n";
+                            }
+                            else
+                                dataSend = "HTTP/1.1 404 Not found\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nContent-Type: text/html\r\n\r\n";
+                        }
+                        if (requested[1].Contains(".css"))
+                        {
+                            if (File.Exists(www + requested[1]))
+                            {
+                                document = File.ReadAllText(www + requested[1]);
+                                length = document.Length;
+                                dataSend = "HTTP/1.1 200 OK\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nLast-Modified: " + File.GetLastWriteTime(www + requested[1]).ToString() + "\r\nContent-length: " + length + "\r\nContent-Type: text/css\r\n\r\n" + document + "\n\n";
+                            }
+                            else
+                                dataSend = "HTTP/1.1 404 Not found\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nContent-Type: text/html\r\n\r\n";
+                        }
+                        if (requested[1].Contains(".js"))
+                        {
+                            if (File.Exists(www + requested[1]))
+                            {
+                                document = File.ReadAllText(www + requested[1]);
+                                length = document.Length;
+                                dataSend = "HTTP/1.1 200 OK\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nLast-Modified: " + File.GetLastWriteTime(www + requested[1]).ToString() + "\r\nContent-length: " + length + "\r\nContent-Type: text/javascript\r\n\r\n" + document + "\n\n";
+                            }
+                            else
+                                dataSend = "HTTP/1.1 404 Not found\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nContent-Type: text/html\r\n\r\n";
+                        }
+                        if (requested[1].Contains(".ico") || requested[1].Contains(".png") || requested[1].Contains(".jpg") || requested[1].Contains(".jpeg"))
+                        {
+                            if (File.Exists(www + requested[1]))
+                            {
+                                document=File.ReadAllText(www + requested[1]);
+                                length = document.Length;
+                                dataSend = "HTTP/1.1 200 OK\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nLast-Modified: " + File.GetLastWriteTime(www + requested[1]).ToString() + "\r\nContent-length: " + new FileInfo(www + requested[1]).Length + "\r\nContent-Type: image/jpeg\r\n\r\n";
+                                client.SendFile(www + requested[1]);
+                            }
+                            else
+                                dataSend = "HTTP/1.1 404 Not found\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nContent-Type: text/html\r\n";
+                        }
+
+                        msg = Encoding.ASCII.GetBytes(dataSend);
+                        client.Send(msg);
+
+                        client.Shutdown(SocketShutdown.Both);
+                        client.Close();
                     }
                 }
-                else
-                {
-                    if (File.Exists(wwwData + requested[1]))
-                    {
-                        line = File.ReadAllText(wwwData + requested[1]);
-                        int lenght = line.Length;
-                        dataSend = "HTTP/1.1 200 OK\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nLast-Modified: " + File.GetLastWriteTime(wwwData + requested[1]).ToString() + "\r\nContent-Lenght: " + lenght + "\r\nKeep-Alive: timeout=10, max=100\r\nConnection: Keep-Alive\r\nContent-Type: text/html\r\n" + line;
-                    }
-                    else
-                        dataSend = "HTTP/1.1 404 Not found\r\nDate: "+DateTime.Now.ToString()+"\r\nServer: WebNett\r\nKeep-Alive: timeout=10, max=100\r\nConnection: Keep-Alive\r\nContent-Type: text/html\r\n";
-
-                }
-
-
-                //dataSend = "HTTP/1.1 200 OK\r\nDate: " + DateTime.Now.ToString() + "\r\nServer: WebNett\r\nLast-Modified: " + File.GetLastWriteTime(wwwData + root).ToString() +
-                //"\r\nContent-Lenght: " + lenght + "\r\nKeep-Alive: timeout=10, max=100\r\nConnection: Keep-Alive\r\nContent-Type: text/html\r\n" + line;
-
-                msg = Encoding.ASCII.GetBytes(dataSend);
-                client.Send(msg);
-
-                client.Shutdown(SocketShutdown.Both);
-                client.Close();
             }
         }
 
 
-        private void btnStart_Click_1(object sender, EventArgs e)
+        public void btnStart_Click_1(object sender, EventArgs e)
         {
-            if (started == false)
+            if (Convert.ToInt32(txtPort.Text) > 0 && Convert.ToInt32(txtPort.Text) < 65536)
             {
-                Listener l = new Listener();
-                Thread ui = new Thread(new ThreadStart(l.startListening));
-                started = true;
-                ui.Start();
-                File.AppendAllText(log, DateTime.Now.ToString() + "\tInfo\tServer Avviato\n");
-                btnStop.Enabled = true;
+                port = Convert.ToInt32(txtPort.Text);
+                if (www != null)
+                {
+                    if (started == false)
+                    {
+                        maxClients = Convert.ToInt32(nClients.Value);
+                        Listener l = new Listener();
+                        Thread ui = new Thread(new ThreadStart(l.startListening));
+                        started = true;
+                        ui.Start();
+
+                        File.AppendAllText(log, DateTime.Now.ToString() + "\tInfo\tServer Avviato\n");
+
+                        btnStop.Enabled = true;
+                        btnBrowse.Enabled = false;
+
+                        lblStatus.Text = "Started";
+                        lblStatus.ForeColor = Color.Green;
+                    }
+                    else
+                        MessageBox.Show("Server già in ascolto!", "!ERRORE!");
+                }
+                else
+                    MessageBox.Show("Il percorso selezionato non è valido!", "!ERRORE!"); 
             }
             else
-            {
-                MessageBox.Show("Server già in ascolto!", "!ERRORE!");
-            }
+                MessageBox.Show("La porta deve essere compresa tra 0 e 65536!", "!ERRORE!");
+            btnStart.Enabled = false;
         }
 
         private void btnStop_Click_1(object sender, EventArgs e)
         {
             Listener l = new Listener();
             l.stopListening();
-            File.AppendAllText(log, DateTime.Now.ToString() + "\tInfo\tServer Stoppato\n");
+            File.AppendAllText(log, DateTime.Now.ToString() + "\tInfo\tServer Fermato\n");
+            started = false;
+
+            btnBrowse.Enabled = true;
+            btnStart.Enabled = true;
+
+            lblStatus.Text = "Stopped";
+            lblStatus.ForeColor = Color.Red;
+
+            btnStop.Enabled = false;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.ShowDialog();
+
+            if (folder.SelectedPath == null)
+                MessageBox.Show("Il percorso selezionato non è valido!", "ERRORE");
+            else if(Directory.GetFiles(folder.SelectedPath, "*.html").Length==0)
+                MessageBox.Show("Il percorso selezionato non è valido!", "ERRORE");
+            else
+            {
+                www = folder.SelectedPath + "/";
+                lblPath.Text = www;
+            }
+        }
+
+        private void btnBrowse_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip ToolTip1 = new ToolTip();
+            ToolTip1.SetToolTip(this.btnBrowse, this.btnBrowse.Text);
+            ToolTip1.Show("Browse", btnBrowse);
         }
     }
 }
